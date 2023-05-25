4db06a2d1276685f5448f493e7b82251badc54d2
Taiyo Koshiba <solluna.1119@gmail.com>
2023-05-25 15:08:19 +0900

    背景色の追加

--- AboutView.swift
+++ AboutView.swift
@@ -11,24 +11,32 @@ import SwiftUI
 // ルール説明画面
 struct AboutView: View {
     var body: some View {
-        VStack {
-            // タイトル表示
-            Text("遊び方")
-                .font(.largeTitle)
-                .fontWeight(.bold)
-                .padding(20)
+        
+        ZStack {
+            // 背景色
+            Color(red: 0, green: 255, blue: 0)
+                .edgesIgnoringSafeArea(.all)
+                .opacity(0.1)
+            
+            VStack {
+                // タイトル表示
+                Text("遊び方")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                    .padding(20)
 
-            Text(
-            """
-            3桁の数字を当てるゲームです。
-            すべて違う数字です。
-            数字と場所の両方が合っている場合はヒット、数字のみ合っている場合はブローと表示します。
-            例えば、正解が123の場合、135は1ヒット、1ブローです。
-            """)
-                .padding(60)
-                .multilineTextAlignment(.leading)
-                .lineLimit(nil)
-            Spacer()
+                Text(
+                """
+                3桁の数字を当てるゲームです。
+                すべて違う数字です。
+                数字と場所の両方が合っている場合はヒット、数字のみ合っている場合はブローと表示します。
+                例えば、正解が123の場合、135は1ヒット、1ブローです。
+                """)
+                    .padding(60)
+                    .multilineTextAlignment(.leading)
+                    .lineLimit(nil)
+                Spacer()
+            }
         }
     }
 }
